0581619e6bced6b0777e957f2a7de2c3fa6aa317
DESKTOP-C3JEQLF\Min <ngotruongjang@gmail.com>
2024-08-05 11:00:52 +0700

    Lab-05 update

--- labs/lab-05/day-07.js
+++ labs/lab-05/day-07.js
@@ -1,19 +1,41 @@
+// MAP
+// const readline = require('readline-sync');
+
+// const givenStr = readline.question("Please input your sentence: ")
+// let individualWords = givenStr.replace(/,/gi, "").split(" ");
+// const groupWordMap = groupWord(individualWords);
+// console.log(groupWordMap);
+
+// function groupWord(individualWords) {
+//     let wordMap = new Map();
+
+//     for (const word of individualWords) {
+//         if (!wordMap.has(word)) {
+//             wordMap.set(word, 1)
+//         } else {
+//             wordMap.set(word, wordMap.get(word) + 1);
+//         }
+//     }
+//     return wordMap;
+// }
+
+
+// OBJECT
 const readline = require('readline-sync');
 
 const givenStr = readline.question("Please input your sentence: ")
-let individualWords = givenStr.replace(/,/gi, "").split(" ");
-const groupWordMap = groupWord(individualWords);
-console.log(groupWordMap);
+const wordCount = groupWords(givenStr)
+console.log(wordCount);
 
-function groupWord(individualWords) {
-    let wordMap = new Map();
-
-    for (const word of individualWords) {
-        if (!wordMap.has(word)) {
-            wordMap.set(word, 1)
+function groupWords(givenStr) {
+    const words = givenStr.replace(/,/gi, "").split(" ");
+    let wordCount = {};
+    for (const word of words) {
+        if (wordCount[word]) {
+            wordCount[word] += 1;
         } else {
-            wordMap.set(word, wordMap.get(word) + 1);
+            wordCount[word] = 1;
         }
     }
-    return wordMap;
-}
+    return wordCount;
+};
